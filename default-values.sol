18bdeb5bf9d94070433121c38e7f8a6a1265ec7f
phaddad <p.haddad.p@gmail.com>
2022-08-20 19:29:50 -0300

    Solidity 0.8 - Default Values

---
+++ default-values.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+// compiler version must be greater than or equal to 0.8.13 and less than 0.9.0
+pragma solidity ^0.8.13;
+
+contract DefaultValues {
+    bool public b; // default = false
+    uint public u; // default = 0
+    int public i; // default = 0
+    address public a; //   default = 0x0000000000000000000000000000000000000000
+    bytes32 public b32; // default = 0x0000000000000000000000000000000000000000000000000000000000000000
+    
+} 
