b1e367b073d84bcd035df0e8a1a70aaa6b29a9e2
phaddad <p.haddad.p@gmail.com>
2022-08-20 19:19:58 -0300

    Solidity 0.8 - Counter

---
+++ counter.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+// compiler version must be greater than or equal to 0.8.13 and less than 0.9.0
+pragma solidity ^0.8.13;
+
+contract Counter {
+    uint public count;
+    function inc() external {
+        count += 1;
+    }   
+    function dec() external {
+        count -= 1;
+    }    
+}
