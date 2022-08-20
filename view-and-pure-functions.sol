0e46721659835991771fe7f6aabc58063f99f966
phaddad <p.haddad.p@gmail.com>
2022-08-19 21:55:44 -0300

    Solidity 0.8 View and Pure Functions

---
+++ view-and-pure-functions.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+// compiler version must be greater than or equal to 0.8.13 and less than 0.9.0
+pragma solidity ^0.8.13;
+
+
+contract ViewAndPureFunctions    {
+    uint public num;
+    function viewFunc() external view returns (uint) {
+        return num;
+    }
+
+    function pureFunc() external pure returns (uint) {
+        return 1;
+    }
+
+    function addToNum(uint x) external view returns (uint) {
+        return num + x;
+    }
+    function add(uint x, uint y) external pure returns (uint) {
+        return y + x;
+    }
+}
