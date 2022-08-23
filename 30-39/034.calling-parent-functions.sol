e2e8d9368cd912b21612e708a55676460373c656
phaddad <p.haddad.p@gmail.com>
2022-08-23 19:04:03 -0300

    Solidity 0.8 - Calling Parent Functions

--- 30-39/034.calling-parent-functions.sol
+++ 30-39/034.calling-parent-functions.sol
@@ -15,7 +15,6 @@ contract E {
 }
  
  contract F is E {
-
     function foo() public virtual override {
         emit Log("F. foo");
         E.foo();
@@ -23,5 +22,29 @@ contract E {
 
     function bar() public virtual override{
         emit Log("F. bar");
+        super.bar();
+    }
+}
+
+ contract G is E {
+    function foo() public virtual override {
+        emit Log("G. foo");
+        E.foo();
+    }
+
+    function bar() public virtual override{
+        emit Log("G. bar");
+        super.bar();
+    }
+}
+
+
+ contract H is F, G {
+    function foo() public override(F, G) {
+        F.foo();
+    }
+
+    function bar() public virtual override(F, G) {
+        super.bar();
     }
 }
